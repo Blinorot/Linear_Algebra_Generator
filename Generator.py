062bc17c1c926a00cbb336303ac1641020c6240c
Blinorot <pmgrin.work@gmail.com>
2020-07-20 00:18:16 +0300

    Creation of project and its base

---
+++ Generator.py
@@ -0,0 +1,4 @@
+# all modules
+import numpy as np
+from sympy.combinatorics import Permutation
+import Matrix.py
---
+++ Matrix.py
@@ -0,0 +1,5 @@
+class Matrix(object):
+    def __init__(self, in_format):
+        self.format = in_format
+        if (in_format == "inverse"):
+            return iverse_gen
