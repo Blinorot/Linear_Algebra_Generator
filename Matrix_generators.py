b65cb29ca8ec5a7e673690e5138370e4131ead76
Blinorot <pmgrin.work@gmail.com>
2020-07-21 20:02:04 +0300

    Upper Matrix generators added

--- Matrix_generators.py
+++ Matrix_generators.py
@@ -1,45 +1,53 @@
 # M for matrix, NZ for non-zero, WZ for with-zero
 
 
-def gen_FirstElementaryMV(size, max_value=5, value=randomNZ(max_value)):
-    result = np.identity(size, dtype=int)
-    index = [randomNonEqualPair(size, size)]
-    result[index[0]][index[1]] = value
+def gen_FirstElementaryM(size, value, first_index, second_index):
+    result = sp.eye(size)
+    result[first_index, second_index] = value
     return result
 
 
-def gen_FirstElementaryM(size, max_value=5):
-    return gen_FirstElementaryMV(size, randomNZ(max_value))
-
-
-def gen_SecondElementaryMV(size, row, column):
-    result = np.identity(size, dtype=int)
-    result[row][row] = 0
-    result[row][column] = 1
-    result[column][column] = 0
-    result[column][row] = 1
+def gen_SecondElementaryM(size, row, column):
+    result = sp.eye(size)
+    result[row, row] = 0
+    result[row, column] = 1
+    result[column, column] = 0
+    result[column, row] = 1
     return result
 
 
-def gen_SecondElementaryM(size):
-    row, column = randomNonEqualPair(size, size)
-    return gen_SecondElementaryMV(size, row, column)
-
-
-def gen_ThirdElementaryV(size, value):
-    result = np.identity(size, dtype=int)
-    index = np.random.randint(size)
-    result[index][index] = value
+def gen_ThirdElementaryM(size, value, index):
+    result = sp.eye(size)
+    result[index, index] = value
     return result
 
 
 def gen_PermutationM(size):
-    result = np.zeroes((size, size), dtype=int)
+    result = sp.zeros(size, size)
     row = 0
     column = 0
     permutation = np.random.permutation(size)
     while (row < size):
-        column = permutation(size)
-        result[row][column] = 1
+        column = permutation[size]
+        result[row, column] = 1
         row += 1
+    return result
+
+
+def gen_UniUpperM(size, random):
+    result = sp.zeros(size)
+    for row in range(size):
+        for column in range(row):
+            result[row, column] = 1 if row == column else random.max_value()
+    return result
+
+
+def genOneZeroUpperM(size, random):
+    result = sp.eye(size)
+    for row in range(size):
+        for column in range(row):
+            if row == column:
+                result[row, column] = np.random.randint(2)
+            else:
+                result[row, column] = random.max_value()
     return result
